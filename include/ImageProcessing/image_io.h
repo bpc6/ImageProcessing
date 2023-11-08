cedf7f15a95a14243eb15efda4d58bcdbb9b3481
Bryan Covell <bpc6@vt.edu>
2023-11-07 20:36:47 -0500

    got CMake config working

---
+++ include/ImageProcessing/image_io.h
@@ -0,0 +1,8 @@
+#ifndef IMAGEPROCESSING_IMAGE_IO_H
+#define IMAGEPROCESSING_IMAGE_IO_H
+
+#include <string>
+
+int** loadImagePixels(const std::string& imageFileName);
+
+#endif  // IMAGEPROCESSING_IMAGE_IO_H
---
+++ src/image_io.cpp
@@ -0,0 +1,3 @@
+#include "ImageProcessing/image_io.h"
+
+int** loadImagePixels(const std::string& imageFileName) { return nullptr; }
