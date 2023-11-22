e05495980aa42ce95e9883e2ffd5967bf7865d03
Bryan Covell <bpc6@vt.edu>
2023-11-22 15:46:08 -0500

    got test passing

--- src/image_io.cpp
+++ src/image_io.cpp
@@ -2,4 +2,30 @@
 
 #include <opencv2/opencv.hpp>
 
-int** loadImagePixels(const std::string& imageFileName) { return nullptr; }
+int** loadImagePixels(const std::string& imageFileName) {
+  // Read the image
+  cv::Mat image = cv::imread(imageFileName, cv::IMREAD_GRAYSCALE);
+
+  if (image.empty()) {
+    std::cerr << "Error: Unable to read the image." << std::endl;
+    return nullptr;
+  }
+
+  // Get image dimensions
+  int rows = image.rows;
+  int cols = image.cols;
+
+  // Create a 2D array to store pixel values
+  int** pixels = new int*[rows];
+  for (int i = 0; i < rows; ++i) {
+    pixels[i] = new int[cols];
+  }
+
+  // Copy pixel values from the OpenCV matrix to the 2D array
+  for (int i = 0; i < rows; ++i) {
+    for (int j = 0; j < cols; ++j) {
+      pixels[i][j] = static_cast<int>(image.at<uchar>(i, j));
+    }
+  }
+  return pixels;
+}
